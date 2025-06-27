796661bd5b72dd277b90a3aadf572bb85e14853c
ke-kaa <amsalukaku122@gmail.com>
2025-06-27 17:15:44 +0300

    Create ServiceCard component. Add some icons

---
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './serviceCard.css'
+
+export default function (props) {
+    return (
+        <div className='text-center flex-column items-center align-center gap-5 justify-items-center '>
+            <img className='service-icon mb-3' src={props.src}/>
+            <h3 className='font-bold text-lg mb-3'>{props.service}</h3>
+            <p className='description-text'>{props.description}</p>
+        </div>
+    )
+}
