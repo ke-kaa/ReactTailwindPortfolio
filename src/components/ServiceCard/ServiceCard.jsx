6a1b819546f5add2319d5b21e1ccf2ebe144b5de
ke-kaa <amsalukaku122@gmail.com>
2025-06-30 00:17:30 +0300

    add transition effect to ServiceCard. migrate local css to tailwindcss

--- src/components/ServiceCard/ServiceCard.jsx
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -3,10 +3,10 @@ import './serviceCard.css'
 
 export default function (props) {
     return (
-        <div className='text-center flex-column items-center align-center gap-5 justify-items-center mb-5'>
-            <img className='service-icon mb-3' src={props.src}/>
+        <div className='text-center flex-column items-center align-center gap-5 justify-items-center m-7 p-5 bg-white/70 rounded-lg shadow-2xl transition-transform duration-500 hover:scale-110'>
+            <img className='service-icon mb-3 w-[40px]' src={props.src}/>
             <h3 className='font-bold text-lg mb-3'>{props.service}</h3>
-            <p className='description-text'>{props.description}</p>
+            <p className='description-text max-w-[370px] mb-2'>{props.description}</p>
         </div>
     )
 }
