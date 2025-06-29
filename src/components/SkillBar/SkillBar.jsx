66c96dcf36651aac472341e0ea445410221e59d1
ke-kaa <amsalukaku122@gmail.com>
2025-06-30 00:16:34 +0300

    add transition to SkillBar

--- src/components/SkillBar/SkillBar.jsx
+++ src/components/SkillBar/SkillBar.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import {React, useState} from 'react'
 import './skillBar.css'
 
 export default function SkillBar(props) {
+    const [hovered, sethovered] = useState(false);
+    
     return (
         <div>
-            <div className="progress-container">
+            <div className="progress-container ">
                 <div className="progress-label">
                     <span>{props.skill}</span>
-                    {/* <span>{props.progress}</span> */}
+                    <span>{hovered && (<span className='ml-2 font-bold'>{props.progress}</span>)}</span>
                 </div>
-                <div className="progress-bar">
-                <div className="progress-fill"
+                <div className="progress-bar transition-transform duration-500 hover:scale-105" onMouseEnter={() => sethovered(true)} onMouseLeave={() => sethovered(false)}>
+                <div className="progress-fill "
                     style={{ width: typeof props.progress === 'number' ? `${props.progress}%` : props.progress }}>
                 </div>
                 </div>
