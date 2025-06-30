0f76fb49b987d25555fe49a2c4dc57540cd7fee2
ke-kaa <amsalukaku122@gmail.com>
2025-06-30 21:46:03 +0300

    set font and scroll-behaviour in app.css, import pages into app.js and rendered them, import tailwind and define dark theme for the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import "@fontsource/poppins"
+import "@fontsource/dancing-script"
+import './App.css'
+import 'aos/dist/aos.css'
+import AOS from 'aos'
+
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import Homepage from './pages/HomePage/Homepage'
+import About from './pages/About/About'
+import ServicePage from './pages/ServicePage/ServicePage'
+import Resume from './pages/Resume/Resume'
+import Contact from './pages/Contact/Contact'
+import Portfolio from './pages/Portfolio/Portfolio'
 
 export default function App() {
+    useEffect(() => {
+        AOS.init({duration:1000})
+    }, []);
     return (
         <>
         <Header />
+        <Homepage />
+        <About />
+        <ServicePage />
+        <Resume />
+        <Portfolio />
+        <Contact />
         <Footer />
         </>
         
