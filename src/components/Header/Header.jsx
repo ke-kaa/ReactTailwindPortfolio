5e314e5e7daa583f0c9cacd0a7e1a08b7a823335
ke-kaa <amsalukaku122@gmail.com>
2025-12-22 12:04:26 +0300

    feat: change profile picture, add google seo, code refactor

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
-import { React, useState, useRef, useEffect } from 'react'
-import './header.css'
+import { React, useState, useRef, useEffect } from "react";
+import "./header.css";
 
-import { MdDarkMode } from 'react-icons/md'
-import { FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa'
-import { MdLightMode } from 'react-icons/md';
+import { MdDarkMode } from "react-icons/md";
+import { FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
+import { MdLightMode } from "react-icons/md";
 
 export default function Header() {
     const [menuVisible, setMenuVisible] = useState(false);
@@ -14,25 +14,23 @@ export default function Header() {
     const menuRef = useRef(null);
     const buttonRef = useRef(null);
 
-    // Hide on scroll down, show on scroll up
     useEffect(() => {
         const handleScroll = () => {
             const currentScrollY = window.scrollY;
             if (currentScrollY > lastScrollY && currentScrollY > 80) {
-                setShowHeader(false); // Hide header
+                setShowHeader(false);
             } else {
-                setShowHeader(true); // Show header
+                setShowHeader(true);
             }
             setLastScrollY(currentScrollY);
 
-            // Close menu if open and user scrolls (on mobile)
             if (menuVisible && window.innerWidth < 1024) {
                 setMenuVisible(false);
                 setStaggered(false);
             }
         };
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
     }, [lastScrollY, menuVisible]);
 
     const handleClickOutside = (e) => {
@@ -47,8 +45,9 @@ export default function Header() {
     };
 
     useEffect(() => {
-        document.addEventListener('mousedown', handleClickOutside);
-        return () => document.removeEventListener('mousedown', handleClickOutside);
+        document.addEventListener("mousedown", handleClickOutside);
+        return () =>
+            document.removeEventListener("mousedown", handleClickOutside);
     }, []);
 
     const handleMenuToggle = () => {
@@ -57,15 +56,15 @@ export default function Header() {
             setTimeout(() => setStaggered(true), 50);
         } else {
             setStaggered(false);
-        };
+        }
     };
     const toggleTheme = () => {
         setIsDark((prev) => {
             const newValue = !prev;
             if (newValue) {
-                document.documentElement.classList.add('dark');
+                document.documentElement.classList.add("dark");
             } else {
-                document.documentElement.classList.remove('dark');
+                document.documentElement.classList.remove("dark");
             }
             return newValue;
         });
@@ -74,7 +73,7 @@ export default function Header() {
     return (
         <div
             className={`NavBarCont shadow fixed top-0 left-0 w-full z-50 bg-white/70 backdrop-blur-md transition-transform duration-300 ${
-                showHeader ? 'translate-y-0' : '-translate-y-full'
+                showHeader ? "translate-y-0" : "-translate-y-full"
             } dark:bg-slate-grey dark:text-marigold`}
         >
             <div className="flex justify-between flex-wrap items-center mx-auto NavBar px-5 w-full">
@@ -84,40 +83,49 @@ export default function Header() {
                 <div className="flex justify-between ">
                     <button
                         ref={buttonRef}
-                        id='more-button'
+                        id="more-button"
                         onClick={handleMenuToggle}
                         className="relative lg:hidden"
-                        aria-label={menuVisible ? 'Close menu' : 'Open menu'}
+                        aria-label={menuVisible ? "Close menu" : "Open menu"}
                         aria-expanded={menuVisible}
                     >
                         <div className="relative flex items-center justify-center w-[50px] h-[50px] duration-200">
                             <div
                                 className={`flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 ${
-                                menuVisible ? '-rotate-[45deg]' : ''
+                                    menuVisible ? "-rotate-[45deg]" : ""
                                 } origin-center`}
                             >
                                 <div
-                                className={`bg-black h-[2px] w-1/2 rounded transform transition-all duration-300 ${
-                                    menuVisible ? '-rotate-90 h-[1px] -translate-y-[1px]' : ''
-                                } origin-right delay-75 dark:bg-marigold`}
+                                    className={`bg-black h-[2px] w-1/2 rounded transform transition-all duration-300 ${
+                                        menuVisible
+                                            ? "-rotate-90 h-[1px] -translate-y-[1px]"
+                                            : ""
+                                    } origin-right delay-75 dark:bg-marigold`}
                                 ></div>
                                 <div className="bg-black h-[2px] rounded dark:bg-marigold"></div>
                                 <div
-                                className={`bg-black h-[2px] w-1/2 rounded self-end transform transition-all duration-300 ${
-                                    menuVisible ? '-rotate-90 h-[1px] translate-y-[1px]' : ''
-                                } origin-left delay-75 dark:bg-marigold`}
+                                    className={`bg-black h-[2px] w-1/2 rounded self-end transform transition-all duration-300 ${
+                                        menuVisible
+                                            ? "-rotate-90 h-[1px] translate-y-[1px]"
+                                            : ""
+                                    } origin-left delay-75 dark:bg-marigold`}
                                 ></div>
                             </div>
                         </div>
                     </button>
 
                     <div
-                        className={`menu ${menuVisible ? 'flex' : 'hidden'} flex-col lg:flex lg:flex-row lg:gap-4 `}
+                        className={`menu ${
+                            menuVisible ? "flex" : "hidden"
+                        } flex-col lg:flex lg:flex-row lg:gap-4 `}
                         id="menu-container"
                         ref={menuRef}
                     >
-                        <ul id="menu" className="flex flex-col lg:flex-row gap-2 items-center align-items-center bg-white dark:bg-slate-grey dark:text-marigold">
-                            { [
+                        <ul
+                            id="menu"
+                            className="flex flex-col lg:flex-row gap-2 items-center align-items-center bg-white dark:bg-slate-grey dark:text-marigold"
+                        >
+                            {[
                                 { label: "HOME", href: "#" },
                                 { label: "ABOUT", href: "#about" },
                                 { label: "SERVICES", href: "#services" },
@@ -128,54 +136,91 @@ export default function Header() {
                                 <li
                                     key={item.label}
                                     style={{
-                                        transition: 'opacity 0.4s, transform 0.4s',
-                                        transitionDelay: menuVisible && staggered ? `${idx * 100}ms` : '0ms',
+                                        transition:
+                                            "opacity 0.4s, transform 0.4s",
+                                        transitionDelay:
+                                            menuVisible && staggered
+                                                ? `${idx * 100}ms`
+                                                : "0ms",
                                         opacity:
-                                          // On large screens, always visible
+                                            // On large screens, always visible
                                             window.innerWidth >= 1024
                                                 ? 1
-                                                // On mobile, use staggered animation
-                                                : (menuVisible ? (staggered ? 1 : 0) : 0),
-                                            transform:
+                                                : // On mobile, use staggered animation
+                                                menuVisible
+                                                ? staggered
+                                                    ? 1
+                                                    : 0
+                                                : 0,
+                                        transform:
                                             window.innerWidth >= 1024
-                                                ? 'none'
-                                                : (menuVisible && staggered ? 'translateY(0)' : 'translateY(20px)'),
+                                                ? "none"
+                                                : menuVisible && staggered
+                                                ? "translateY(0)"
+                                                : "translateY(20px)",
+                                    }}
+                                >
+                                    <a
+                                        href={item.href}
+                                        onClick={() => {
+                                            if (window.innerWidth < 1024)
+                                                setMenuVisible(false);
                                         }}
                                     >
-                                        <a
-                                            href={item.href}
-                                            onClick={() => {
-                                                if (window.innerWidth < 1024) setMenuVisible(false);
-                                            }}
-                                            >
-                                            {item.label}
-                                        </a>
+                                        {item.label}
+                                    </a>
                                 </li>
                             ))}
                         </ul>
                     </div>
 
-                    <div id="social-links" className='flex justify-between items-center gap-1'>
-                        <a href="https://www.linkedin.com/kaku-amsalu" className="inline" target='_blank'>
-                            {isDark ? <FaLinkedin size={22} color='slate-gray'/> : <FaLinkedin size={22}/>}
+                    <div
+                        id="social-links"
+                        className="flex justify-between items-center gap-1"
+                    >
+                        <a
+                            href="https://www.linkedin.com/kaku-amsalu"
+                            className="inline"
+                            target="_blank"
+                        >
+                            {isDark ? (
+                                <FaLinkedin size={22} color="slate-gray" />
+                            ) : (
+                                <FaLinkedin size={22} />
+                            )}
                         </a>
-                        <a href="https://github.com/ke-kaa" target='_blank'>
-                            {isDark ? <FaGithub size={22} color='slate-gray'/> : <FaGithub size={22}/>}
+                        <a href="https://github.com/ke-kaa" target="_blank">
+                            {isDark ? (
+                                <FaGithub size={22} color="slate-gray" />
+                            ) : (
+                                <FaGithub size={22} />
+                            )}
                         </a>
-                        <a href="https://www.linkedin.com/in/kaku-amsalu-5aa926283/" target='_blank'>
-                            {isDark ? <FaInstagram size={22} color='slate-gray'/> : <FaInstagram size={22}/>}
+                        <a
+                            href="https://www.linkedin.com/in/kaku-amsalu-5aa926283/"
+                            target="_blank"
+                        >
+                            {isDark ? (
+                                <FaInstagram size={22} color="slate-gray" />
+                            ) : (
+                                <FaInstagram size={22} />
+                            )}
                         </a>
                     </div>
                     <button
-                    className="flex items-center align-items-center mx-4"
-                    onClick={toggleTheme}
-                    aria-label="Toggle dark mode"
-                    type="button"
+                        className="flex items-center align-items-center mx-4"
+                        onClick={toggleTheme}
+                        aria-label="Toggle dark mode"
+                        type="button"
                     >
-                        {isDark ? <MdLightMode size={24} /> : <MdDarkMode size={24}/>}
+                        {isDark ? (
+                            <MdLightMode size={24} />
+                        ) : (
+                            <MdDarkMode size={24} />
+                        )}
                     </button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -1,78 +1,106 @@
-import React, {useState, useEffect} from 'react';
-import { FaArrowDown } from 'react-icons/fa';
-import profileImage from '../../assets/profile/profile.jpg';
-import newProf from '../../assets/profile/newProf.JPG';
-import './homepage.css'
+import React, { useState, useEffect } from "react";
+import { FaArrowDown } from "react-icons/fa";
+import profileImage from "../../assets/profile/profile.jpg";
+import newProf from "../../assets/profile/newProf.JPG";
+import "./homepage.css";
 
 const TITLES = [
-  "SOFTWARE ENGINEER",
-  "FRONTEND DEVELOPER",
-  "BACKEND DEVELOPER",
-  "FULL STACK DEVELOPER"
+    "SOFTWARE ENGINEER",
+    "FRONTEND DEVELOPER",
+    "BACKEND DEVELOPER",
+    "FULL STACK DEVELOPER",
 ];
 
 export default function () {
-  const [currentTitle, setCurrentTitle] = useState(0);
-  const [displayedText, setDisplayedText] = useState('');
-  const [isDeleting, setIsDeleting] = useState(false);
+    const [currentTitle, setCurrentTitle] = useState(0);
+    const [displayedText, setDisplayedText] = useState("");
+    const [isDeleting, setIsDeleting] = useState(false);
 
-  useEffect(() => {
-    let typingSpeed = 100;
-    let timeout;
+    useEffect(() => {
+        let typingSpeed = 100;
+        let timeout;
 
-    const fullText = TITLES[currentTitle];
+        const fullText = TITLES[currentTitle];
 
-    if (isDeleting) {
-      typingSpeed = 50;
-      timeout = setTimeout(() => {
-        setDisplayedText(fullText.substring(0, displayedText.length - 1));
-        if (displayedText.length - 1 === 0) {
-          setIsDeleting(false);
-          setCurrentTitle((prev) => (prev + 1) % TITLES.length);
+        if (isDeleting) {
+            typingSpeed = 50;
+            timeout = setTimeout(() => {
+                setDisplayedText(
+                    fullText.substring(0, displayedText.length - 1)
+                );
+                if (displayedText.length - 1 === 0) {
+                    setIsDeleting(false);
+                    setCurrentTitle((prev) => (prev + 1) % TITLES.length);
+                }
+            }, typingSpeed);
+        } else {
+            timeout = setTimeout(() => {
+                setDisplayedText(
+                    fullText.substring(0, displayedText.length + 1)
+                );
+                if (displayedText.length + 1 === fullText.length) {
+                    setTimeout(() => setIsDeleting(true), 1500); // Pause before deleting
+                }
+            }, typingSpeed);
         }
-      }, typingSpeed);
-    } else {
-      timeout = setTimeout(() => {
-        setDisplayedText(fullText.substring(0, displayedText.length + 1));
-        if (displayedText.length + 1 === fullText.length) {
-          setTimeout(() => setIsDeleting(true), 1500); // Pause before deleting
-        }
-      }, typingSpeed);
-    }
 
-    return () => clearTimeout(timeout);
-  }, [displayedText, isDeleting, currentTitle]);
+        return () => clearTimeout(timeout);
+    }, [displayedText, isDeleting, currentTitle]);
 
-  useEffect(() => {
-    setDisplayedText('');
-  }, [currentTitle]);
+    useEffect(() => {
+        setDisplayedText("");
+    }, [currentTitle]);
 
-  return (
-    <div data-aos="fade-up"className='homePage py-20 min-h-screen flex items-center justify-center bg-[#d2d2d2] dark:bg-dark-void dark:text-marigold'>
-      <div className='w-full max-w-6xl flex flex-col lg:flex-row-reverse items-center justify-center lg:gap-10'>
-        <div data-aos="fade-up">
-          <div id="profile-img"  className='rounded-full w-80 h-80 rounded overflow-hidden justify-self-center mb-20'>
-            <img src={newProf}/>
-          </div>
+    return (
+        <div
+            data-aos="fade-up"
+            className="homePage py-20 min-h-screen flex items-center justify-center bg-[#d2d2d2] dark:bg-dark-void dark:text-marigold"
+        >
+            <div className="w-full max-w-6xl flex flex-col lg:flex-row-reverse items-center justify-center lg:gap-10">
+                <div data-aos="fade-up">
+                    <div
+                        id="profile-img"
+                        className="rounded-full w-80 h-80 rounded overflow-hidden justify-self-center mt-10"
+                    >
+                        <img src={newProf} />
+                    </div>
+                </div>
+                <div data-aos="fade-up" id="intro" className="text-center">
+                    <h1 className="text-2xl p-3 dark:text-marigold">
+                        HI, I'M <br />
+                    </h1>
+                    <div className="my-name text-5xl font-bold my-5">
+                        KAKU AMSALU
+                    </div>
+                    <span className="animated-title block text-2xl mb-3">
+                        {displayedText}&nbsp;
+                    </span>
+                    <p className="px-3">
+                        Aspiring Full-Stack Developer | Passionate About Web
+                        Development & AI
+                    </p>
+                    <div
+                        id="work-contact"
+                        className="my-10 md:flex md:flex-row md:gap-2 md:justify-self-center md:gap-10"
+                    >
+                        <a
+                            href="#portfolio"
+                            id="view-my-work"
+                            className="portfolio-link block justify-self-center bg-black text-white p-3 transition-transform duration-300 hover:scale-110 focus:scale-105 dark:text-marigold dark:bg-dusty-grey rounded"
+                        >
+                            View My Works
+                        </a>
+                        <a
+                            href="#contact"
+                            id="contact-me"
+                            className="contact-me underline flex items-center justify-self-center h-full justify-center my-auto transition-transform duration-300 hover:scale-110 focus:scale-105 focus:outline-none dark:text-marigold dark:focus:text-liquid-lova"
+                        >
+                            <span className="mr-1">Contact me</span>
+                            <FaArrowDown size={16} className="animate-bounce" />
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div data-aos="fade-up" id="intro" className='text-center'>
-          <h1 className='text-2xl p-3 dark:text-marigold'>
-            HI, I'M <br/>
-          </h1>
-          <div className="my-name text-5xl font-bold my-5">
-            KAKU AMSALU
-          </div>
-          <span className='animated-title block text-2xl mb-3'>{displayedText}&nbsp;</span>
-          <p className='px-3'>Aspiring Full-Stack Developer | Passionate About Web Development & AI</p>
-          <div id="work-contact" className='my-10 md:flex md:flex-row md:gap-2 md:justify-self-center md:gap-10'>
-            <a href='#portfolio' id="view-my-work" className='portfolio-link block justify-self-center bg-black text-white p-3 transition-transform duration-300 hover:scale-110 focus:scale-105 dark:text-marigold dark:bg-dusty-grey rounded'>View My Works</a>
-            <a href='#contact' id="contact-me" className="contact-me underline flex items-center justify-self-center h-full justify-center my-auto transition-transform duration-300 hover:scale-110 focus:scale-105 focus:outline-none dark:text-marigold dark:focus:text-liquid-lova">
-              <span className='mr-1'>Contact me</span>
-              <FaArrowDown  size={16} className='animate-bounce'/>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+    );
 }
